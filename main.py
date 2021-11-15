3c03660da8c3891738198e859e0f6130eb6b7544
zirth <zirth66@gmail.com>
2021-11-15 16:11:03 +0100

    Added flip_card function

--- main.py
+++ main.py
@@ -1,35 +1,60 @@
 from tkinter import *
+import pandas as pd
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
 
+df = pd.read_csv("data/french_words.csv")
+to_learn = df.to_dict(orient='records')
+current_card = {}
+
+# ---------------------------- Functions ------------------------------- #
+
+def next_card(): 
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(card_background, image=card_front_img)
+    flip_timer = window.after(3000, func=flip_card)
 
 
+def flip_card():
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_background, image=card_back_img)
+    
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Flashcard App")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
-
-answer_image = PhotoImage(file="images/card_front.png")
+flip_timer = window.after(3000, func=flip_card)
 
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 card_front_img = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=card_front_img)
-canvas.create_text(400, 150, text="Title", font="Arial 40 italic")
-canvas.create_text(400, 263, text="Word", font="Arial 60 bold")
+card_back_img = PhotoImage(file="images/card_back.png")
+card_background = canvas.create_image(400, 263, image=card_front_img)
+card_title = canvas.create_text(400, 150, text="", font="Arial 40 italic")
+card_word = canvas.create_text(400, 263, text="", font="Arial 60 bold")
 canvas.grid(column=0, row=0, columnspan=2)
+
+
+
+
 
 
 # Buttons
 wrong_image = PhotoImage(file="images/wrong.png")
-wrong_button = Button(image=wrong_image, highlightthickness=0)
+wrong_button = Button(image=wrong_image, highlightthickness=0, command=next_card)
 wrong_button.grid(column=0, row=1)
 
 right_image = PhotoImage(file="images/right.png")
-right_button = Button(image=right_image, highlightthickness=0)
+right_button = Button(image=right_image, highlightthickness=0, command=next_card)
 right_button.grid(column=1, row=1)
 
-
+next_card()
 
 
 
